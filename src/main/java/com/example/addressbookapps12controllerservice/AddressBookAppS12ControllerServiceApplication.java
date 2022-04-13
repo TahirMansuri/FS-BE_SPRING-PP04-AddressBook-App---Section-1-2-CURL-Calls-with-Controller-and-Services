01c3dc645816262a7fc1bbbe23e586f605872361
TahirMansuri <tprocks786@gmail.com>
2022-04-13 18:28:04 +0530

    MODIFIED: Setup DEV PROD STAGGING Application Properties files

--- src/main/java/com/example/addressbookapps12controllerservice/AddressBookAppS12ControllerServiceApplication.java
+++ src/main/java/com/example/addressbookapps12controllerservice/AddressBookAppS12ControllerServiceApplication.java
@@ -3,14 +3,15 @@ package com.example.addressbookapps12controllerservice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class AddressBookAppS12ControllerServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookAppS12ControllerServiceApplication.class, args);
-        log.info("Addressbook App Service is Started.");
+        ApplicationContext context =  SpringApplication.run(AddressBookAppS12ControllerServiceApplication.class, args);
+        log.info("Addressbook App Service is Started in {} Environment.",context.getEnvironment().getActiveProfiles());
     }
 
 }
