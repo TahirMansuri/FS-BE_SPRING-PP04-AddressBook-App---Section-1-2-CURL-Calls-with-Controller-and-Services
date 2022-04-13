d63593c8af0380c74f53adce1f89c0ca64dc38d1
TahirMansuri <tprocks786@gmail.com>
2022-04-13 18:16:22 +0530

    MODIFIED: Setup SLF4J Logging using Lombok

--- src/main/java/com/example/addressbookapps12controllerservice/AddressBookAppS12ControllerServiceApplication.java
+++ src/main/java/com/example/addressbookapps12controllerservice/AddressBookAppS12ControllerServiceApplication.java
@@ -1,13 +1,16 @@
 package com.example.addressbookapps12controllerservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookAppS12ControllerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AddressBookAppS12ControllerServiceApplication.class, args);
+        log.info("Addressbook App Service is Started.");
     }
 
 }
